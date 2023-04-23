aa191b63ac90d64f1e3720a9700321a2f751048a
MrFlexEc <itsthebryanyt@gmail.com>
2023-04-23 17:55:14 -0500

    Alertas al guardar datos, y seleccionar imagenes de la galeria OK

--- app/src/main/java/com/example/calofit/Agregar_Paciente.kt
+++ app/src/main/java/com/example/calofit/Agregar_Paciente.kt
@@ -1,11 +1,75 @@
 package com.example.calofit
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat
+import com.example.calofit.databinding.ActivityAgregarPacienteBinding
 
 class Agregar_Paciente : AppCompatActivity() {
+    private lateinit var binding: ActivityAgregarPacienteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_paciente)
+        binding = ActivityAgregarPacienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{ requestPermission() }
+
+        val simpleDialog = AlertDialog.Builder(this)
+            .setTitle("Plato registrado")
+            .setMessage("El plato se guardo exitosamente")
+            .setPositiveButton("Aceptar"){_,_ ->
+                Toast.makeText(this, "Plato registrado", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+
+        binding.button2.setOnClickListener{simpleDialog.show()}
+    }
+
+    private fun requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )== PackageManager.PERMISSION_GRANTED->{
+                    pickPhotoFromGallery()
+                }
+                else-> requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            pickPhotoFromGallery()
+        }
+    }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if (isGranted){
+            pickPhotoFromGallery()
+        }else{
+            Toast.makeText(this, "You need to enable the permission", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val startForActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = result.data?.data
+            binding.imageView.setImageURI(data)
+        }
+    }
+    private fun pickPhotoFromGallery(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForActivityGallery.launch(intent)
     }
 }
--- app/src/main/java/com/example/calofit/Agregar_Platos.kt
+++ app/src/main/java/com/example/calofit/Agregar_Platos.kt
@@ -1,11 +1,73 @@
 package com.example.calofit
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.calofit.databinding.ActivityAgregarPlatosBinding
 
 class Agregar_Platos : AppCompatActivity() {
+    private lateinit var binding: ActivityAgregarPlatosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_platos)
+        binding = ActivityAgregarPlatosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button3.setOnClickListener{requestPermission()}
+
+        val simpleDialog = AlertDialog.Builder(this)
+            .setTitle("Plato registrado")
+            .setMessage("El plato se guardo exitosamente")
+            .setPositiveButton("Aceptar"){_,_ ->
+                Toast.makeText(this, "Plato registrado", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+
+        binding.button4.setOnClickListener{simpleDialog.show()}
+    }
+
+    private fun requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )== PackageManager.PERMISSION_GRANTED->{
+                    pickPhotoFromGallery()
+                }
+                else-> requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            pickPhotoFromGallery()
+        }
+    }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if (isGranted){
+            pickPhotoFromGallery()
+        }else{
+            Toast.makeText(this, "You need to enable the permission", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val startForActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = result.data?.data
+            binding.imageView2.setImageURI(data)
+        }
+    }
+    private fun pickPhotoFromGallery(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForActivityGallery.launch(intent)
     }
 }
