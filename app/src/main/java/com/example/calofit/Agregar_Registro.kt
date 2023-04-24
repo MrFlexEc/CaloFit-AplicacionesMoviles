94e260da0d201b4425eda8fb7c60d5ac2d648595
MrFlexEc <itsthebryanyt@gmail.com>
2023-04-23 19:42:29 -0500

    Ejemplo de Combobox creado en pantalla de registro

--- app/src/main/java/com/example/calofit/Agregar_Registro.kt
+++ app/src/main/java/com/example/calofit/Agregar_Registro.kt
@@ -2,10 +2,31 @@ package com.example.calofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 
 class Agregar_Registro : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_registro)
+
+        val items = listOf("Material", "Design", "Components", "Android")
+
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
+
+        val adapter = ArrayAdapter(this,R.layout.list_item, items)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+                adapterView, view, i, l ->
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this, "Item: $itemSelected", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
