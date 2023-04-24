32f5b91169f6cf751087086c88daed3e59c002ff
MrFlexEc <itsthebryanyt@gmail.com>
2023-04-23 19:47:05 -0500

    Alerta de Registro guardado creado

--- app/src/main/java/com/example/calofit/Agregar_Registro.kt
+++ app/src/main/java/com/example/calofit/Agregar_Registro.kt
@@ -1,10 +1,12 @@
 package com.example.calofit
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.Toast
 
 class Agregar_Registro : AppCompatActivity() {
@@ -13,6 +15,20 @@ class Agregar_Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_registro)
+
+        var btnSave:Button = findViewById(R.id.button5)
+
+        val simpleDialog = AlertDialog.Builder(this)
+            .setTitle("Plato registrado")
+            .setMessage("El plato se guardo exitosamente")
+            .setPositiveButton("Aceptar"){_,_ ->
+                Toast.makeText(this, "Plato registrado", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+
+        btnSave.setOnClickListener{simpleDialog.show()}
+
+
 
         val items = listOf("Material", "Design", "Components", "Android")
 
