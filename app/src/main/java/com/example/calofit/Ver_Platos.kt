ce04cbb376f81bbf5d25a1a932c20ef830ee5821
Nagely Chancay <e1350176721@live.uleam.edu.ec>
2023-04-24 01:35:21 -0500

    Creacion de Interfaces Ver datos

---
+++ app/src/main/java/com/example/calofit/Ver_Platos.kt
@@ -0,0 +1,11 @@
+package com.example.calofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ver_Platos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_platos)
+    }
+}
---
+++ app/src/main/java/com/example/calofit/Ver_Registro.kt
@@ -0,0 +1,11 @@
+package com.example.calofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ver_Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_registro)
+    }
+}
